0ef3af93f4adcd5e0cbfa77536cb64950465c53f
llwwns <llwwns@gmail.com>
2015-04-01 01:14:37 +0000

    Add install.sh

---
+++ install.sh
@@ -0,0 +1,3 @@
+curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
+mkdir -p -v ~/.vim/bundle
+git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
