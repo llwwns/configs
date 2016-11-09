03acdbe5fdab0234842b90e0677be197fef66aa2
llwwns <llwwns@gmail.com>
2016-11-09 11:30:10 +0900

    Fix configs

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto" --depth=1
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-git clone https://github.com/lukechilds/zsh-nvm ~/.oh-my-zsh/custom/plugins/zsh-nvm --depth=1
+git clone https://github.com/lukechilds/zsh-nvm.git ~/.zsh-nvm --depth=1
 git clone https://github.com/yyuu/pyenv.git ~/.pyenv --depth=1
 
