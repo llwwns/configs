acd2d12e11ea0c98c0eec5dd977eabd73c85b477
llwwns <llwwns@gmail.com>
2016-11-29 12:34:21 +0900

    Add git configs

--- install.sh
+++ install.sh
@@ -3,4 +3,18 @@ git clone git@github.com:zsh-users/antigen.git ~/.antigen --depth=1
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 #git clone https://github.com/lukechilds/zsh-nvm.git ~/.zsh-nvm --depth=1
 git clone https://github.com/yyuu/pyenv.git ~/.pyenv --depth=1
+git config --global core.editor nvim
+git config --global gui.encoding utf-8
+git config --global diff.tool vimdiff
+git config --global merge.tool vimdiff
+git config --global merge.conflictstyle diff3
+git config --global mergetool.vimdiff.cmd "nvim -d \$BASE \$LOCAL \$REMOTE \$MERGED -c '\$wincmd w' -c 'wincmd J'"
+git config --global difftool.prompt false
+git config --global mergetool.keepBackup false
+git config --global alias.st status
+git config --global alias.cm commit
+git config --global alias.fh fetch
+git config --global alias.mg merge
+git config --global alias.ck checkout
+git config --global oh-my-zsh.hide-dirty 1
 
