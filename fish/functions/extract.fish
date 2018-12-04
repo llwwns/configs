e5f9f5c677336dc363cef2d0022f87c61479a4be
Wei Li <llwwns@gmail.com>
2018-12-05 00:33:45 +0900

    Add extract

---
+++ fish/functions/extract.fish
@@ -0,0 +1,29 @@
+function extract
+  switch $argv[1]
+  case '*.tar.gz' '*.tgz'
+    tar xzvf $argv[1]
+  case '*.tar.xz'
+    tar Jxvf $argv[1]
+  case '*.zip'
+    unzip $argv[1]
+  case '*.lzh' lha e $argv[1]
+  case '*.tar.bz2' '*.tbz'
+    tar xjvf $argv[1]
+  case '*.tar.Z'
+    tar zxvf $argv[1]
+  case '*.gz'
+    gzip -d $argv[1]
+  case '*.bz2'
+    bzip2 -dc $argv[1]
+  case '*.Z'
+    uncompress $argv[1]
+  case '*.tar'
+    tar xvf $argv[1]
+  case '*.arj'
+    unarj $argv[1]
+  case '*.7z'
+    7z e $argv[1]
+  case '*.rar'
+    unrar e $argv[1]
+  end
+end
