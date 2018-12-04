14e3475b2cc687495d26184d4602741fd0c0136c
Wei Li <llwwns@gmail.com>
2018-12-04 22:41:36 +0900

    Fix fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,12 +1,7 @@
-set PATH $PATH \
-  $HOME/platform-tools \
-  $HOME/go/bin \
-  $HOME/.gem/ruby/2.5.0/bin \
-  $HOME/.yarn/bin \
-  $HOME/fzf/bin
-set -x FFF_OPENER /usr/sbin/wslview
-set -x DOCKER_HOST tcp://localhost:2375
-set -x NNN_USE_EDITOR 1
+if test -e $HOME/.fishenv
+  source $HOME/.fishenv
+end
+set PATH $PATH $HOME/fzf/bin
 set -x FZF_FIND_FILE_COMMAND 'fd'
 set -x FZF_CTRL_T_COMMAND "$FZF_DEFAULT_COMMAND"
 #fish_vi_key_bindings
