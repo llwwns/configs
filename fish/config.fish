7b78ba7b264bf4e000423fb48768229c07eb6556
Wei Li <llwwns@gmail.com>
2018-12-08 12:05:25 +0900

    Fix fish

--- fish/config.fish
+++
@@ -1,7 +0,0 @@
-if test -e $HOME/.fishenv
-  source $HOME/.fishenv
-end
-set PATH $PATH $HOME/fzf/bin
-set -x FZF_FIND_FILE_COMMAND 'fd'
-set -x FZF_CTRL_T_COMMAND "$FZF_DEFAULT_COMMAND"
-#fish_vi_key_bindings
