11cc2dc964946b8c6c62cf02b0db148f0b89b27f
li.wei <li.wei@donuts.ne.jp>
2019-09-10 19:22:25 +0900

    Fix xmonad

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -15,12 +15,13 @@ import XMonad.ManageHook
 import XMonad.Layout.SubLayouts
 import XMonad.Layout.WindowNavigation
 import XMonad.Layout.BoringWindows
+import XMonad.Layout.Dwindle
 --  modMask = mod4Mask -- use the Windows button as mod
 main = xmonad $ desktopConfig
     {
       terminal = "konsole"
-    , focusFollowsMouse = False
-    , clickJustFocuses = True
+    , focusFollowsMouse = True
+    , clickJustFocuses = False
     , layoutHook = myLayoutHook
     , manageHook = manageDocks <+> myManageHook <+> manageHook desktopConfig
     , focusedBorderColor = "#009cb5"
@@ -36,8 +37,8 @@ main = xmonad $ desktopConfig
     -- ((mod1Mask .|. controlMask, xK_u), withFocused (sendMessage . UnMerge)),
     -- ((mod1Mask, xK_bracketleft), onGroup W.focusUp'),
     -- ((mod1Mask, xK_bracketright), onGroup W.focusDown')]
- 
-myLayoutHook = avoidStruts  $ Responsive MFocus ||| Responsive (MDouble SHorizontally) ||| Responsive (MDouble SVertically) ||| Full
+myLayoutHook = avoidStruts $ Responsive(Dwindle R CW (5/2) (11/10)) ||| Full
+-- myLayoutHook = avoidStruts  $ Responsive MFocus ||| Responsive (MDouble SHorizontally) ||| Responsive (MDouble SVertically) ||| Responsive emptyBSP ||| Full
 -- myLayoutHook = avoidStruts $ windowNavigation $ subTabbed $ boringWindows $ Responsive MFocus ||| Responsive (MDouble SHorizontally) ||| Responsive (MDouble SVertically) ||| Full
 
 data MFocus a = MFocus deriving ( Read, Show )
@@ -46,8 +47,8 @@ newtype Responsive l a = Responsive (l a) deriving (Show, Read)
 
 -- Mirror for vertical monitor
 instance LayoutClass l a => LayoutClass (Responsive l) a where
-    runLayout (W.Workspace i (Responsive l) ms) r = (map (second responsive) *** fmap Responsive)
-                                                `fmap` runLayout (W.Workspace i l ms) (responsive r)
+    runLayout (W.Workspace i (Responsive l) ms) r = (fmap (second responsive) *** fmap Responsive)
+                                                <$> runLayout (W.Workspace i l ms) (responsive r)
                                                     where
                                                         responsive = case r of
                                                                         Rectangle _ _ sw sh | sw < sh -> mirrorRect
@@ -56,42 +57,42 @@ instance LayoutClass l a => LayoutClass (Responsive l) a where
     handleMessage (Responsive l) = fmap (fmap Responsive) . handleMessage l
     description (Responsive l) = "Responsive "++ description l
 
--- Display focus window in main area and others on right side
-instance LayoutClass MFocus Window where
-    pureLayout _ sc ws = zip ups tops ++ [(W.focus ws, mainPane)] ++ zip dns bottoms
-     where
-       ups    = reverse $ W.up ws
-       dns    = W.down ws
-       count  = (length ups) + (length dns)
-       (mainArea,  subArea) = splitHorizontallyBy (7%8) sc
-       (top, other)         = splitVerticallyBy   ((8*length ups)%(count*8 + 1)) subArea
-       (_, bottom)          = splitVerticallyBy   (1%(length dns*8 + 1)) other
-       mainPane | ups /= [] || dns /= [] = mainArea
-                | otherwise              = sc
-       tops    = if ups /= [] then splitVertically (length ups) top    else []
-       bottoms = if dns /= [] then splitVertically (length dns) bottom else []
-
-data SplitD = SHorizontally | SVertically deriving ( Read, Show )
-
-data MDouble a = MDouble SplitD deriving ( Read, Show )
-
--- Display first two window in mainarea and others on right side
-instance LayoutClass MDouble Window where
-  pureLayout (MDouble sd) sc ws = zip all rts
-     where
-       ups    = reverse $ W.up ws
-       dns    = W.down ws
-       all    = ups ++ [W.focus ws] ++ dns
-       split  = case sd of
-                  SHorizontally -> splitHorizontally 2
-                  SVertically -> splitVertically 2
-       rts    = case all of
-                  []        -> []
-                  [_]       -> [sc]
-                  [_, _]    -> split sc
-                  otherwise -> (split x) ++ (splitVertically (length all - 2) y)
-                      where
-                        (x, y) = splitHorizontallyBy (7%8) sc
+-- -- Display focus window in main area and others on right side
+-- instance LayoutClass MFocus Window where
+--     pureLayout _ sc ws = zip ups tops ++ [(W.focus ws, mainPane)] ++ zip dns bottoms
+--      where
+--        ups    = reverse $ W.up ws
+--        dns    = W.down ws
+--        count  = (length ups) + (length dns)
+--        (mainArea,  subArea) = splitHorizontallyBy (7%8) sc
+--        (top, other)         = splitVerticallyBy   ((8*length ups)%(count*8 + 1)) subArea
+--        (_, bottom)          = splitVerticallyBy   (1%(length dns*8 + 1)) other
+--        mainPane | ups /= [] || dns /= [] = mainArea
+--                 | otherwise              = sc
+--        tops    = if ups /= [] then splitVertically (length ups) top    else []
+--        bottoms = if dns /= [] then splitVertically (length dns) bottom else []
+--
+-- data SplitD = SHorizontally | SVertically deriving ( Read, Show )
+--
+-- data MDouble a = MDouble SplitD deriving ( Read, Show )
+--
+-- -- Display first two window in mainarea and others on right side
+-- instance LayoutClass MDouble Window where
+--   pureLayout (MDouble sd) sc ws = zip all rts
+--      where
+--        ups    = reverse $ W.up ws
+--        dns    = W.down ws
+--        all    = ups ++ [W.focus ws] ++ dns
+--        split  = case sd of
+--                   SHorizontally -> splitHorizontally 2
+--                   SVertically -> splitVertically 2
+--        rts    = case all of
+--                   []        -> []
+--                   [_]       -> [sc]
+--                   [_, _]    -> split sc
+--                   otherwise -> (split x) ++ (splitVertically (length all - 2) y)
+--                       where
+--                         (x, y) = splitHorizontallyBy (7%8) sc
 
 myManageHook = composeAll  
   [ className =? "plasmashell" --> doFloat -- doIgnore
