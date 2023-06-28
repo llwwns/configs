9357de56a27448f42d79ba56eef5e0df6b4b2d5f
llwwns <llwwns@gmail.com>
2023-06-28 17:47:31 +0900

    add queries

---
+++ nvim/after/queries/elixir/highlights.scm
@@ -0,0 +1,8 @@
+; extends
+(pair key: (keyword) @spell)
+((atom) @spell)
+((quoted_content) @spell)
+(arguments (alias) @spell)
+(arguments (identifier) @spell)
+(call target: (identifier) @spell)
+((binary_operator) left: (identifier) @spell)
---
+++ nvim/after/queries/ruby/highlights.scm
@@ -0,0 +1,10 @@
+; extends
+(class name: (constant) @spell)
+(assignment left: (constant) @spell)
+(assignment left: (identifier) @spell)
+(method name: (identifier) @spell)
+(match_pattern pattern: (identifier) @spell)
+(test_pattern pattern: (identifier) @spell)
+
+
+
