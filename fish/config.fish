f95b451009e28fd1e835b9b246c8764f5d9bee3f
llwwns <llwwns@gmail.com>
2025-05-08 15:43:31 +0900

    fix fish

--- fish/config.fish
+++ fish/config.fish
@@ -4,10 +4,13 @@ bind -M normal \cv edit_command_buffer
 #    test -z "$fish_private_mode"
 #    and command zoxide add -- (__zoxide_pwd)
 #end
-set -g fish_function_path $HOME/configs/fish/functions $fish_function_path
+#set -g fish_function_path $HOME/configs/fish/functions $fish_function_path
 set -x EDITOR nvim
 set -x GOPATH $XDG_DATA_HOME/go
 set fish_greeting
 zoxide init fish | source
 #set __zoxide_z_prefix 'z!'
 #complete --command z --no-files --arguments '(__zoxide_z_complete)'
+abbr --add sl ls
+abbr --add vi nvim
+abbr --add view nvim -R
--- fish/setup.fish
+++ fish/setup.fish
@@ -1,7 +1,7 @@
 set -U EDITOR nvim
-abbr --add sl ls
-abbr --add vi nvim
-abbr --add view nvim -R
+#abbr --add sl ls
+#abbr --add vi nvim
+#abbr --add view nvim -R
 
 set -Ux XDG_CONFIG_HOME $HOME/.config
 set -Ux XDG_CACHE_HOME $HOME/.cache
@@ -15,14 +15,20 @@ set -Ux PNPM_HOME $XDG_DATA_HOME/pnpm
 set -Ux ASDF_DIR /opt/asdf-vm
 set -Ux ASDF_DATA_DIR $XDG_DATA_HOME/asdf
 
+mkdir -p $HOME/.local/bin
 fish_add_path -U $HOME/.local/bin
+mkdir -p  $CARGO_HOME/bin
 fish_add_path -U $CARGO_HOME/bin
+mkdir -p  $HOME/.deno/bin
 fish_add_path -U $HOME/.deno/bin
+mkdir -p  $GOPATH/bin
 fish_add_path -U $GOPATH/bin
+mkdir -p  $PNPM_HOME
 fish_add_path -U $PNPM_HOME
+mkdir -p  $ASDF_DATA_DIR/shims
 fish_add_path -U $ASDF_DATA_DIR/shims
 
-set -U fish_function_path $fish_function_path $HOME/configs/fish/functions
+#set -U fish_function_path $HOME/configs/fish/functions $fish_function_path
 
 set -xU FZF_DEFAULT_COMMAND fd
 set -xU FZF_FIND_FILE_COMMAND fd
